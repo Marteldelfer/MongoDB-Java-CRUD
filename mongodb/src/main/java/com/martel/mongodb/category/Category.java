30085f496f7c3052627e564ee31321f16cb6fbbc
Murillo Ferraz <marteldelfer@gmail.com>
2024-09-24 01:14:00 -0300

    Add DBRef

--- mongodb/src/main/java/com/martel/mongodb/category/Category.java
+++ mongodb/src/main/java/com/martel/mongodb/category/Category.java
@@ -1,14 +1,20 @@
 package com.martel.mongodb.category;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
+@Document
 @Data
 @AllArgsConstructor
 @Builder
 public class Category {
 
+    @Id
+    private String id;
     private String name;
     private String description;
 
--- mongodb/src/main/java/com/martel/mongodb/product/Product.java
+++ mongodb/src/main/java/com/martel/mongodb/product/Product.java
@@ -3,6 +3,7 @@ package com.martel.mongodb.product;
 import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.martel.mongodb.category.Category;
@@ -24,6 +25,7 @@ public class Product {
 
     private List<String> tags;
 
+    @DBRef
     private Category category;
     
 }
